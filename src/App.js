b6046379fc0ec74a72ef8ac9266293e8c4b7ef0e
klhelker <57188023+klhelker@users.noreply.github.com>
2020-04-18 18:45:47 -0500

    added cards and putting together routes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import CharCard from "./components/CharCard"
+import NavBar from "./components/NavBar";
+import Wrapper from "./components/Wrapper";
+import characters from "./characters.json";
+class App extends Component {
 
-function App() {
+  state = {
+    characters
+  };
+
+ render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+        <Wrapper>
+          <NavBar>Clicky Game</NavBar>
+          {this.state.characters.map(friend => (
+            <CharCard
+              id={characters.id}
+              key={characters.id}
+              image={friend.image}
+            />
+          ))}
+          
+        </Wrapper>
+      );
+          }
+        }
 
 export default App;
---
+++ src/components/NavBar/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./style.css";
+
+const NavBar = () => (
+ <nav className="navbar">
+     <div className="navbar--logo-holder">
+         <h1>Clicky Game</h1>
+         </div>
+    <ul className="navbar--link">
+        <li className="navbar--link-item">click image to begin</li>
+        <li className="navbar--link-item">score:</li>
+        
+    </ul>
+ </nav>
+)
+
+
+export default NavBar
