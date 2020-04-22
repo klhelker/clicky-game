f1fee9a515802f4d4d6af275d967ca3c60bf0bef
klhelker <57188023+klhelker@users.noreply.github.com>
2020-04-22 18:33:45 -0500

    fixing style.css

--- client/src/components/NavBar/index.js
+++ client/src/components/NavBar/index.js
@@ -4,11 +4,10 @@ import "./style.css";
 const NavBar = () => (
  <nav className="navbar navbar-sticky">
      <div className="navbar--logo-holder">
-         <h1>Clicky Game</h1>
+         <h1>Simpson's Memory Game</h1>
          </div>
     <ul className="navbar--link">
-        <li className="navbar--link-item">click image to begin</li>
-        <li className="navbar--link-item">score:</li>
+        <li className="navbar--link-item">Click image to begin!</li>
         
     </ul>
  </nav>
