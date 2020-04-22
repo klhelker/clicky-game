79e037b50aee0d5920eb6df4658337c1c6bc5784
klhelker <57188023+klhelker@users.noreply.github.com>
2020-04-22 17:24:47 -0500

    heroku npm start

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
   }
   
-app.use("/", (req, res) =>
-  res.sendFile(path.join(__dirname, "../client/build/index.html"))
+app.use("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "./client/build/index.html"))
 )
 
 app.listen(PORT, () =>
